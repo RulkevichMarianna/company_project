6c61af4feaa5e36ae7fc8a891cf9332dadf943e4
Maryana Rulkevich <obrazcova.m@gmail.com>
2022-03-14 01:21:30 +0300

    in work

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import json
 
 from pydantic import BaseModel
-from datetime import datetime
 from uuid import UUID
 
 
@@ -18,9 +17,9 @@ class Item(BaseModel):
 
 
 class Order(BaseModel):
-    user: dict
-    created_at: datetime
-    items: list
+    user: User
+    created_at: str
+    items: list[Item]
 
 
 if __name__ == "__main__":
@@ -32,12 +31,15 @@ if __name__ == "__main__":
         if command == "SAVE":
             with open('input.json') as json_string:
                 save = json.load(json_string)
-            user = User(**save)
-            created_at = str(save['createdAt'])
-            item = Item(**save)
-            print(user)
+                order = Order(**save)
+                user_login = order.user.login
+                print(user_login)
+                print("Данные сохранены")
         if command == "ORDERS":
-            def orders():
+            user = order.user
+            for item in user:
+                if item == "superLogin":
+                    new_dict = { }
                 pass
         if command == "TOTAL_PRICE":
             pass
